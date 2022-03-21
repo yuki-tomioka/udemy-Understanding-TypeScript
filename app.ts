d03f424f5db17d3da2a78f2d8b01d61e7c4f73a0
Yuki Tomioka <yuki.tomioka@zozo.com>
2022-03-21 19:49:08 +0900

    型推論

--- app.ts
+++ app.ts
@@ -10,9 +10,11 @@ function add(n1: number, n2: number, showResult: boolean, phrase: string) {
   }
 }
 
-const number1 = 5;
+let number1: number;
+number1 = 5;
 const number2 = 2.8;
 const printResult = true;
-const ResultPhrase = "Result: ";
+let ResultPhrase = "Result: ";
+// ResultPhrase = 0;
 
 add(number1, number2, printResult, ResultPhrase);
