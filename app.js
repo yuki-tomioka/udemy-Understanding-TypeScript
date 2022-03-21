5e2ae700b587ec0c89c1432d15a13158cc7793df
Yuki Tomioka <yuki.tomioka@zozo.com>
2022-03-21 20:17:16 +0900

    Tuple型

--- app.js
+++ app.js
@@ -5,8 +5,12 @@
 var person = {
     name: "yota",
     age: 30,
-    hobbies: ["Sports", "Cooking"]
+    hobbies: ["Sports", "Cooking"],
+    role: [2, "author"]
 };
+// person.role.push("admin");
+// person.role[1] = 10;
+// person.role = [0, "admin", "user"];
 var favoriteActivities;
 favoriteActivities = ["Sports"];
 console.log(person.name);
--- app.ts
+++ app.ts
@@ -2,11 +2,22 @@
 //   name: string;
 //   age: number;
 // }
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: "yota",
   age: 30,
   hobbies: ["Sports", "Cooking"],
+  role: [2, "author"],
 };
+
+// person.role.push("admin");
+// person.role[1] = 10;
+
+// person.role = [0, "admin", "user"];
 
 let favoriteActivities: string[];
 favoriteActivities = ["Sports"];
