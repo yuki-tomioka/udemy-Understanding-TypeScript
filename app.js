6681108728700cef4edf75dcf432ee0ba2dd4c57
Yuki Tomioka <yuki.tomioka@zozo.com>
2022-03-21 19:45:24 +0900

    number, string, boolean型の使い方

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-function add(n1, n2) {
+function add(n1, n2, showResult, phrase) {
     // if (typeof n1 !== "number" || typeof n2 !== "number") {
     //   throw new Error("入力値が正しくありません");
     // }
-    return n1 + n2;
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return n1 + n2;
+    }
 }
-var number1 = "5";
+var number1 = 5;
 var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
+var printResult = true;
+var ResultPhrase = "Result: ";
+add(number1, number2, printResult, ResultPhrase);
--- app.ts
+++ app.ts
@@ -1,12 +1,18 @@
-function add(n1: number, n2: number) {
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
   // if (typeof n1 !== "number" || typeof n2 !== "number") {
   //   throw new Error("入力値が正しくありません");
   // }
-  return n1 + n2;
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return n1 + n2;
+  }
 }
 
 const number1 = 5;
 const number2 = 2.8;
+const printResult = true;
+const ResultPhrase = "Result: ";
 
-const result = add(number1, number2);
-console.log(result);
+add(number1, number2, printResult, ResultPhrase);
