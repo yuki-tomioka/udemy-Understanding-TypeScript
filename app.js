df30ff51a91d3a33c7f6e2f2d8b04680b449a751
Yuki Tomioka <yuki.tomioka@zozo.com>
2022-03-21 20:11:42 +0900

    Array型

--- app.js
+++ app.js
@@ -4,6 +4,14 @@
 // }
 var person = {
     name: "yota",
-    age: 30
+    age: 30,
+    hobbies: ["Sports", "Cooking"]
 };
+var favoriteActivities;
+favoriteActivities = ["Sports"];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+    // console.log(hobby.map());
+}
