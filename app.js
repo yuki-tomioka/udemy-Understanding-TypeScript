c4ae7cdbb02ee7ef2c7cf66e13f006aca0755620
Yuki Tomioka <yuki.tomioka@zozo.com>
2022-03-21 19:56:39 +0900

    Object型

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
-function add(n1, n2, showResult, phrase) {
-    // if (typeof n1 !== "number" || typeof n2 !== "number") {
-    //   throw new Error("入力値が正しくありません");
-    // }
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return n1 + n2;
-    }
-}
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var ResultPhrase = "Result: ";
-add(number1, number2, printResult, ResultPhrase);
+// const person: {
+//   name: string;
+//   age: number;
+// }
+var person = {
+    name: "yota",
+    age: 30
+};
+console.log(person.name);
---
+++ app.ts
@@ -0,0 +1,10 @@
+// const person: {
+//   name: string;
+//   age: number;
+// }
+const person = {
+  name: "yota",
+  age: 30,
+};
+
+console.log(person.name);
