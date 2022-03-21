b62eeac792238c870fa52d78d247aedcf47d6695
Yuki Tomioka <yuki.tomioka@zozo.com>
2022-03-21 20:25:58 +0900

    enum型

--- app.ts
+++ app.ts
@@ -2,16 +2,33 @@
 //   name: string;
 //   age: number;
 // }
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "yota",
+//   age: 30,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author"],
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {
+  ADMIN = "ADMIN",
+  READ_ONLY = 100,
+  AUTHOR,
+}
+
+const person = {
   name: "yota",
   age: 30,
   hobbies: ["Sports", "Cooking"],
-  role: [2, "author"],
+  role: Role.ADMIN,
 };
 
 // person.role.push("admin");
@@ -27,4 +44,8 @@ console.log(person.name);
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
   // console.log(hobby.map());
+}
+
+if (person.role === Role.ADMIN) {
+  console.log("管理者ユーザー");
 }
