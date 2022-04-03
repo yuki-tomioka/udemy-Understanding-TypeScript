d7aeded276af3da7c48f5f5a06792e00059ee24c
Yuki Tomioka <yuki.tomioka@zozo.com>
2022-04-03 21:29:58 +0900

    メソッドの追加

--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,24 @@
 "use strict";
 var Department = (function () {
     function Department(n) {
+        this.employees = [];
         this.name = n;
     }
     Department.prototype.describe = function () {
         console.log('Department: ' + this.name);
     };
+    Department.prototype.addEmployee = function (employee) {
+        this.employees.push(employee);
+    };
+    Department.prototype.printEmployeeInformation = function () {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    };
     return Department;
 }());
 var accounting = new Department('Accounting');
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+accounting.employees[2] = "Anna";
 accounting.describe();
-var accountingCopy = { name: 'DUMMY', describe: accounting.describe };
-accountingCopy.describe();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 class Department {
   name: string;
+  employees: string[] = [];
 
   constructor(n:string) {
     this.name = n;
@@ -8,12 +9,26 @@ class Department {
   describe(this: Department) {
     console.log('Department: ' + this.name)
   }
+
+  addEmployee(employee: string){
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length)
+    console.log(this.employees)
+  }
 }
 
 const accounting =  new Department('Accounting');
 
+accounting.addEmployee('Max')
+accounting.addEmployee('Manu')
+
+accounting.employees[2] = "Anna"
+
 accounting.describe()
 
-const accountingCopy = {name: 'DUMMY', describe: accounting.describe };
+// const accountingCopy = {name: 'DUMMY', describe: accounting.describe };
 
-accountingCopy.describe();
+// accountingCopy.describe();
