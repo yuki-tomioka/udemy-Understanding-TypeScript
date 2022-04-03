a5e436b2832c3e1f1dc467c10b3eb80494f95382
Yuki Tomioka <yuki.tomioka@zozo.com>
2022-04-03 23:32:52 +0900

    Sction6の準備

--- dist/app.js
+++ dist/app.js
@@ -1,27 +1,2 @@
 "use strict";
-var add;
-add = function (n1, n2) {
-    return n1 + n2;
-};
-var Person = (function () {
-    function Person(n) {
-        this.age = 30;
-        if (n) {
-            this.name = n;
-        }
-    }
-    Person.prototype.greet = function (phrase) {
-        if (this.name) {
-            console.log(phrase + " " + this.name);
-        }
-        else {
-            console.log("Hi!");
-        }
-    };
-    return Person;
-}());
-var user1;
-user1 = new Person();
-user1.greet("Hello I am");
-console.log(user1);
 //# sourceMappingURL=app.js.map
