a243ec7b3784f6a8b88e854e359f1db1f8f8eb81
Yuki Tomioka <yuki.tomioka@zozo.com>
2022-04-03 23:54:45 +0900

    型ガード

--- dist/app.js
+++ dist/app.js
@@ -4,4 +4,49 @@ var e1 = {
     privileges: ["create-server"],
     startDate: new Date(),
 };
+function add(a, b) {
+    if (typeof a === "string" || typeof b === "string") {
+        return a.toString() + b.toLocaleString();
+    }
+    return a + b;
+}
+function printEmployeeInformation(emp) {
+    console.log(emp.name);
+    if ("privileges" in emp) {
+        console.log("Privileges: " + emp.privileges);
+    }
+    if ("startDate" in emp) {
+        console.log("startDate: " + emp.startDate);
+    }
+}
+printEmployeeInformation({ name: "Manu", startDate: new Date() });
+var Car = (function () {
+    function Car() {
+    }
+    Car.prototype.drive = function () {
+        console.log("運転中...");
+    };
+    return Car;
+}());
+var Truck = (function () {
+    function Truck() {
+    }
+    Truck.prototype.drive = function () {
+        console.log("トラックを運転中...");
+    };
+    Truck.prototype.loadCargo = function (amount) {
+        console.log("荷物を載せています..." + amount);
+    };
+    return Truck;
+}());
+var v1 = new Car();
+var v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -17,6 +17,61 @@ const e1: ElevatedEmployee = {
 };
 
 type Combinable = string | number;
-type Numeric =  number | boolean;
+type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toLocaleString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log(emp.name);
+  if ("privileges" in emp) {
+    console.log("Privileges: " + emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log("startDate: " + emp.startDate);
+  }
+}
+
+printEmployeeInformation({ name: "Manu", startDate: new Date() });
+
+class Car {
+  drive() {
+    console.log("運転中...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("トラックを運転中...");
+  }
+
+  loadCargo(amount: number) {
+    console.log("荷物を載せています..." + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  // if ("loadCargo" in vehicle) {
+  //   vehicle.loadCargo(1000);
+  // }
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
