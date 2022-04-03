3ac3b39227c84aab17401fb58e9050a5cd1c72e8
Yuki Tomioka <yuki.tomioka@zozo.com>
2022-04-03 21:40:03 +0900

    readonlyの追加

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 class Department {
-  // private id: string
+  // private readonly id: string
   // name: string;
   private employees: string[] = [];
 
-  constructor(private id:string, public name:string) {
+  constructor(private readonly id:string, public name:string) {
     // this.id = id;
     // this.name = n;
   }
@@ -13,6 +13,7 @@ class Department {
   }
 
   addEmployee(employee: string){
+    this.id = 'd2';
     this.employees.push(employee);
   }
 
