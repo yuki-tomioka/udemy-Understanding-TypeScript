e841724b258bb6c8bf607cf4c4bad0a4cbd01ad8
Yuki Tomioka <yuki.tomioka@zozo.com>
2022-04-03 23:20:37 +0900

    関数型としてのインターフェイス

--- dist/app.js
+++ dist/app.js
@@ -1,16 +1,27 @@
 "use strict";
+var add;
+add = function (n1, n2) {
+    return n1 + n2;
+};
 var Person = (function () {
     function Person(n) {
         this.age = 30;
-        this.name = n;
+        if (n) {
+            this.name = n;
+        }
     }
     Person.prototype.greet = function (phrase) {
-        console.log(phrase + " " + this.name);
+        if (this.name) {
+            console.log(phrase + " " + this.name);
+        }
+        else {
+            console.log("Hi!");
+        }
     };
     return Person;
 }());
 var user1;
-user1 = new Person("max");
+user1 = new Person();
 user1.greet("Hello I am");
 console.log(user1);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,45 @@
+// type AddFn = (a: number, b: number) => number;
+interface AddFn {
+  (a: number, b: number): number;
+}
+
+let add: AddFn;
+
+add = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
 interface Named {
-  readonly name: string;
+  readonly name?: string;
+  outputName?: string;
 }
 
 interface Greetable extends Named {
-
   greet(phrase: string): void;
 }
 
 class Person implements Greetable {
-  name: string;
+  name?: string;
   age = 30;
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(n?: string) {
+    if (n) {
+      this.name = n;
+    }
   }
 
   greet(phrase: string) {
-    console.log(phrase + " " + this.name);
+    if(this.name) {
+      console.log(phrase + " " + this.name);
+    } else {
+      console.log("Hi!");
+    }
   }
 }
 
 let user1: Greetable;
 
-user1 = new Person("max");
+user1 = new Person();
 // user1.name = "";
 
 user1.greet("Hello I am");
