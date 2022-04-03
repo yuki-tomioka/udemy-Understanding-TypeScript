141f9752034dbd41203b4ba0df5986769d521aa1
Yuki Tomioka <yuki.tomioka@zozo.com>
2022-04-03 21:06:29 +0900

    Sction5の準備

--- dist/app.js
+++ dist/app.js
@@ -1,14 +1,2 @@
 "use strict";
-let userInput;
-let userName;
-userInput = 5;
-userInput = "Max";
-if (typeof userInput === "string") {
-    userName = userInput;
-}
-function generateError(message, code) {
-    throw { message: message, errorCode: code };
-}
-const result = generateError("エラーが発生しました", 500);
-console.log(result);
 //# sourceMappingURL=app.js.map
