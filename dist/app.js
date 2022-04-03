adfdb2a1fba6006496b0b9bee4dbb85b608430ca
Yuki Tomioka <yuki.tomioka@zozo.com>
2022-04-03 23:00:10 +0900

    interfaceの作成

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,11 @@
 "use strict";
+var user1;
+user1 = {
+    name: "Max",
+    age: 30,
+    greet: function (phrase) {
+        console.log(phrase + " " + this.name);
+    },
+};
+user1.greet("Hello I am");
 //# sourceMappingURL=app.js.map
