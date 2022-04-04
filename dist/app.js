2f77b0c3aa3d3aac71c8216f5c76878ff81749b7
Yuki Tomioka <yuki.tomioka@zozo.com>
2022-04-04 19:05:12 +0900

    Genericsに制約を追加

--- dist/app.js
+++ dist/app.js
@@ -3,5 +3,5 @@ function merge(objA, objB) {
     return Object.assign(objA, objB);
 }
 var mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
-console.log(mergedObj.age);
+console.log(mergedObj.name);
 //# sourceMappingURL=app.js.map
