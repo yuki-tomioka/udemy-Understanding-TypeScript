0a868c5a8bf8b51e5c6941002c354433201682ea
Yuki Tomioka <yuki.tomioka@zozo.com>
2022-04-04 19:16:21 +0900

    keyofの制約

--- dist/app.js
+++ dist/app.js
@@ -12,4 +12,8 @@ function countAndDescribe(element) {
     return [element, descriptionText];
 }
 console.log(countAndDescribe(["Sports", "Cooking"]));
+function extractAndConvert(obj, key) {
+    return "Value: " + obj[key];
+}
+extractAndConvert({ name: "Max" }, "name");
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -31,3 +31,12 @@ function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
 }
 
 console.log(countAndDescribe(["Sports", "Cooking"]));
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return "Value: " + obj[key];
+}
+
+extractAndConvert({ name: "Max" }, "name");
