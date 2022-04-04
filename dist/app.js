bb2c420f3587e6e1508b21aaff49b87bf9f7c54b
Yuki Tomioka <yuki.tomioka@zozo.com>
2022-04-04 18:05:48 +0900

    Sction7の準備

--- dist/app.js
+++ dist/app.js
@@ -1,28 +1,2 @@
 "use strict";
-var _a;
-var e1 = {
-    name: "Max",
-    privileges: ["create-server"],
-    startDate: new Date(),
-};
-function add(a, b) {
-    if (typeof a === "string" || typeof b === "string") {
-        return a.toString() + b.toLocaleString();
-    }
-    return a + b;
-}
-var result = add("Hello", "TypeScript");
-result.split("");
-var fetchedUserData = {
-    id: "u1",
-    name: "user1",
-    job: {
-        title: "Developer",
-        description: "TypeScript",
-    },
-};
-console.log((_a = fetchedUserData === null || fetchedUserData === void 0 ? void 0 : fetchedUserData.job) === null || _a === void 0 ? void 0 : _a.title);
-var userInput = undefined;
-var storedData = userInput !== null && userInput !== void 0 ? userInput : "DEFAULT";
-console.log(storedData);
 //# sourceMappingURL=app.js.map
