68b8ea68b9a73856737f0a5969534fc7478e3d0c
Yuki Tomioka <yuki.tomioka@zozo.com>
2022-04-04 18:24:02 +0900

    Generic関数の作成

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,7 @@
 "use strict";
-var names = [];
-names[0].split(" ");
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
+}
+var mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
+console.log(mergedObj.age);
 //# sourceMappingURL=app.js.map
