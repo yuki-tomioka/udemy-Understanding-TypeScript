5b13306f78c94eb4ae3eba8ffd7e709f6ad8f974
Yuki Tomioka <yuki.tomioka@zozo.com>
2022-06-19 23:15:08 +0900

    Axios & 住所の座標取得

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,36 @@
+import axios from "axios";
+
 const form = document.querySelector("form")!;
 const addressInput = document.getElementById("address") as HTMLInputElement;
 
 const GOOGLE_API_KEY = "AIzaSyBDjYFBiCHJPeVLvkjVoJc_OyIljiXvhew";
 
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
+
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
+  console.log(enteredAddress);
 
-  // Google API に送信
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then((response) => {
+      if (response.data.status !== "OK") {
+        throw new Error("座標を取得できませんでした。");
+      }
+      const coordinates = response.data.results[0].geometry.location;
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
 }
 
 form.addEventListener("submit", searchAddressHandler);
