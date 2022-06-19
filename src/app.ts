4511fac99584d60ce16a0e9ae090023b7a04ba5d
Yuki Tomioka <yuki.tomioka@zozo.com>
2022-06-19 22:41:16 +0900

    ユーザー入力の取得

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-// ここにコードを書きます
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address") as HTMLInputElement;
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  // Google API に送信
+}
+
+form.addEventListener("submit", searchAddressHandler);
