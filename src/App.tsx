15a9dad80d698559533e19d82dc4b5ab486156b9
Yuki Tomioka <yuki.tomioka@zozo.com>
2022-06-26 03:23:40 +0900

    コンポーネント間の連携

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,13 @@ import NewTodo from "./components/NewTodo";
 
 const App: React.FC = () => {
   const todos = [{ id: "t1", text: "TypeScriptコースの完了" }];
+  const todoAddHandler = (text: string) => {
+    console.log(text);
+  };
 
   return (
     <div className="App">
-      <NewTodo />
+      <NewTodo onAddTodo={todoAddHandler} />
       <TodoList items={todos} />
     </div>
   );
