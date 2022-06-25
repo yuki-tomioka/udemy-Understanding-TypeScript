e207c8c63776c342b6609b14242001fb151e0476
Yuki Tomioka <yuki.tomioka@zozo.com>
2022-06-26 03:33:10 +0900

    State 更新処理の改善

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,10 @@ import { Todo } from "./todo.model";
 const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
   const todoAddHandler = (text: string) => {
-    setTodos([{ id: Math.random().toString(), text: text }]);
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text },
+    ]);
   };
 
   return (
