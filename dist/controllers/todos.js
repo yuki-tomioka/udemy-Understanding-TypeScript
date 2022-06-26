04b69138b3d4bd7cfd194d1d0ef0b3ea3575913d
Yuki Tomioka <yuki.tomioka@zozo.com>
2022-06-26 16:54:15 +0900

    RUD処理の追加

--- dist/controllers/todos.js
+++ dist/controllers/todos.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.createTodo = void 0;
+exports.deleteTodo = exports.updateTodo = exports.getTodos = exports.createTodo = void 0;
 const todo_1 = require("../models/todo");
 const TODOS = [];
 const createTodo = (req, res, next) => {
@@ -10,3 +10,28 @@ const createTodo = (req, res, next) => {
     res.json({ message: "TODOを作成しました。", createTodo: newTodo });
 };
 exports.createTodo = createTodo;
+const getTodos = (req, res, next) => {
+    res.json({ todos: TODOS });
+};
+exports.getTodos = getTodos;
+const updateTodo = (req, res, next) => {
+    const todoId = req.params.id;
+    const updateText = req.body.text;
+    const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+    if (todoIndex < 0) {
+        throw new Error("対象のTODOが見つかりませんでした。");
+    }
+    TODOS[todoIndex] = new todo_1.Todo(todoId, updateText);
+    res.json({ message: "更新しました。", updateTodo: TODOS[todoIndex] });
+};
+exports.updateTodo = updateTodo;
+const deleteTodo = (req, res, next) => {
+    const todoId = req.params.id;
+    const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+    if (todoIndex < 0) {
+        throw new Error("対象のTODOが見つかりませんでした。");
+    }
+    TODOS.splice(todoIndex, 1);
+    res.json({ message: "TODOを削除しました。" });
+};
+exports.deleteTodo = deleteTodo;
--- src/controllers/todos.ts
+++ src/controllers/todos.ts
@@ -12,3 +12,36 @@ export const createTodo: RequestHandler = (req, res, next) => {
 
   res.json({ message: "TODOを作成しました。", createTodo: newTodo });
 };
+
+export const getTodos: RequestHandler = (req, res, next) => {
+  res.json({ todos: TODOS });
+};
+
+export const updateTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const todoId = req.params.id;
+  const updateText = (req.body as { text: string }).text;
+
+  const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+
+  if (todoIndex < 0) {
+    throw new Error("対象のTODOが見つかりませんでした。");
+  }
+
+  TODOS[todoIndex] = new Todo(todoId, updateText);
+
+  res.json({ message: "更新しました。", updateTodo: TODOS[todoIndex] });
+};
+
+export const deleteTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const todoId = req.params.id;
+
+  const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+
+  if (todoIndex < 0) {
+    throw new Error("対象のTODOが見つかりませんでした。");
+  }
+
+  TODOS.splice(todoIndex, 1);
+
+  res.json({ message: "TODOを削除しました。" });
+};
